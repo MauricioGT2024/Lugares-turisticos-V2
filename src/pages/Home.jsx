74d6088412c81b8f260b283d757537735b339096
MauricioGT2024 <senasilvana192@gmail.com>
2025-04-09 14:20:34 +0000

    Upgrade

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { places } from '../data/home';
 import { useColorMode } from '@chakra-ui/react';
-import { FaMapMarkedAlt, FaArrowRight } from 'react-icons/fa';
+import { FaArrowRight } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 
 const DestinationCard = memo(({ place }) => {
